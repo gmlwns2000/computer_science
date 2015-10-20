2cd697dd2535b1d94ac528424600e2dabd21ecd4
Hee Jun Lee <19hlee@iM1213D02.local>
2015-10-20 08:20:03 +0200

    Add Just Counting Project

---
+++ Just_Counting/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+untitled.py
+
+Created by Hee Jun Lee on 2015-10-18.
+Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+loot=int(input("loot: "))
+adder=int(input("adder: "))
+times=int(input("times: "))
+a=loot
+b=0
+while b<times:
+	b=b+1
+	a=a+adder
+	print("["+str(b)+"/"+str(times)+"]"+"("+str(b/times*100)+"%) "+str(a))
+
