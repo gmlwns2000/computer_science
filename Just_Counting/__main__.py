df4efbd70d7f871ed7dad4d77ab02a6875e14db7
Hee Jun Lee <19hlee@iM1213D02.local>
2015-10-20 08:58:59 +0200

    edit just counting project

--- Just_Counting/__main__.py
+++ Just_Counting/__main__.py
@@ -10,13 +10,14 @@ Copyright (c) 2015 __MyCompanyName__. All rights reserved.
 import sys
 import os
 
-loot=int(input("loot: "))
-adder=int(input("adder: "))
-times=int(input("times: "))
-a=loot
-b=0
-while b<times:
-	b=b+1
-	a=a+adder
-	print("["+str(b)+"/"+str(times)+"]"+"("+str(b/times*100)+"%) "+str(a))
+while true:
+        loot=int(input("loot: "))
+        adder=int(input("adder: "))
+        times=int(input("times: "))
+        a=loot
+        b=0
+        while b<times:
+        	b=b+1
+        	a=a+adder
+                print("["+str(b)+"/"+str(times)+"]"+"("+str(b/times*100)+"%) "+str(a))
 
