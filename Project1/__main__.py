ac75c1c3b1030f6726de759fa2981d7f42207b28
Hee Jun Lee <19hlee@iM1213D07.local>
2015-10-25 14:02:07 +0200

    Please work...

---
+++ Project1/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__main__.py
+
+Created by Hee Jun Lee on 2015-10-25.
+Copyright (c) 2015 AinL. All rights reserved.
+"""
+
+print("I'll use py3 not py2.7")
+
+name="Heejun Lee"
+favorite="So funny So nice Introduction of Computer Science."
+hut=77
+
+while 1>0:
+        print("Name: "+name)
+        print("Favorite: "+favorite)
+        print("HUT#: "+str(hut))
