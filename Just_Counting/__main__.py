13d64fe808f4cecaccd3594c2537ef3bc94adf27
AinL <gmlwns5176@gmail.com>
2015-10-26 13:24:06 +0200

    Oh math end!
    dd

--- Just_Counting/__main__.py
+++ Just_Counting/__main__.py
@@ -11,6 +11,9 @@ import sys
 import os
 import time
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 line=95
 
 while True:
@@ -23,6 +26,7 @@ while True:
     while b<times:
        	b=b+1
        	a=a+adder
+       	cls()
         print("["+str(b)+"/"+str(times)+"]"+"("+str(float(b)/float(times)*100)+"%) Now Value: "+str(a))
         v=0
         progress_bar="["
@@ -34,19 +38,22 @@ while True:
             progress_bar=progress_bar+"-"
             v=v+1
         print(progress_bar+"]")
-	
+
+    cls()
     print("===========================================================================")
-    print("=                ===   =====   ====  =====  ===   =====                   =")
-    print("=                =  =  =      =      =      =  =    =                     =")
-    print("=                =   = =      =      =      =   =   =                     =")
-    print("=                =  =  =====   ===   =====  =  =    =                     =")
-    print("=                ===   =          =  =      ===     =                     =")
-    print("=                =  =  =          =  =      =  =    =                     =")
-    print("=                =   = =====  ====   =====  =   =   =                     =")
+    print("=                ===   =====   ====  =   =  =     =====                   =")
+    print("=                =  =  =      =      =   =  =       =                     =")
+    print("=                =   = =      =      =   =  =       =                     =")
+    print("=                =  =  =====   ===   =   =  =       =                     =")
+    print("=                ===   =          =  =   =  =       =                     =")
+    print("=                =  =  =          =  =   =  =       =                     =")
+    print("=                =   = =====  ====    ===   =====   =                     =")
     print("===========================================================================")
     print("")
     print("Result Value(loot+adder*times): "+str(a))
     print("Take Time(ms): "+str(int(time.time()*100)-int(start_time*100))+"ms")
-    print("SPA): "+str(float(int(time.time()*100)-int(start_time*100))/times))
+    print("How long take per add: "+str(float(int(time.time()*100)-int(start_time*100))/times))
     print("")
     print("==========================E  N  D==========================================")
+    input("Press ENTER!!!!!!!!!!!!!!!!!")
+    cls()
---
+++ Snakes/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__main__.py
+
+Created by Hee Jun Lee on 2015-10-25.
+Copyright (c) 2015 AinL. All rights reserved.
+"""
+
+room=1
+
+def room1:
+        print("what the")
+
+def main:
+        room1()
+
+while True:
+        main()
